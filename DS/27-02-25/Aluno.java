5259d20d664985619d7b3d1e3f19eb1d52cc1d63
Jonatas Santos Gandra <jonatassgandra@gmail.com>
2025-02-27 18:26:13 -0300

    aula java

--- DS/27-02-25/Aluno.java
+++ DS/27-02-25/Aluno.java
@@ -1,9 +1,25 @@
-
+// esta é uma classe de objeto
 public class Aluno {
-    // esta é uma classe de objeto
+
     int rm;
-    String nome;
+    private String nome;
     String email;
 
-    
+    public Aluno() {
+    }
+
+    public Aluno(int rm, String nome, String email) {
+
+        this.rm = rm;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome.toUpperCase();
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
--- DS/27-02-25/Main.java
+++ DS/27-02-25/Main.java
@@ -1,12 +1,16 @@
-
 //esta é a classe driver
-public class Main{
+public class Main {
+
     public static void main(String[] args) {
-        
+
         Aluno aluno1 = new Aluno();
         aluno1.rm = 20240015;
-        aluno1.nome = "Jônatas";
+        aluno1.setNome("Jota");
         aluno1.email = "jonatassgandra@gmail.com";
+        Aluno aluno2 = new Aluno(20230054, "Victor", "victor.silva@etec.sp.gov.br");
 
+        System.out.println(aluno1.rm);
+        System.out.println(aluno1.getNome());
     }
+
 }
