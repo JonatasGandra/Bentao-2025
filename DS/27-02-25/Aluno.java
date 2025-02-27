6e557c3cb1cd7a08d2e99bd0e5f7fd0560fa2eff
Jonatas Santos Gandra <jonatassgandra@gmail.com>
2025-02-27 17:32:57 -0300

    primeiro objeto

---
+++ DS/27-02-25/Aluno.java
@@ -0,0 +1,9 @@
+
+public class Aluno {
+    // esta é uma classe de objeto
+    int rm;
+    String nome;
+    String email;
+
+    
+}
---
+++ DS/27-02-25/Main.java
@@ -0,0 +1,12 @@
+
+//esta é a classe driver
+public class Main{
+    public static void main(String[] args) {
+        
+        Aluno aluno1 = new Aluno();
+        aluno1.rm = 20240015;
+        aluno1.nome = "Jônatas";
+        aluno1.email = "jonatassgandra@gmail.com";
+
+    }
+}
