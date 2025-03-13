8381b7fe3595163ebac8d4b9e53eea331b450f8c
Jonatas Santos Gandra <jonatassgandra@gmail.com>
2025-03-13 13:05:41 -0300

    cifra e login

---
+++ PW/Trabalho1/Login/script.js
@@ -0,0 +1,34 @@
+const user = document.querySelector('#usuario')
+const senha = document.querySelector('#senha')
+const erroLogin = document.querySelector('#erro')
+const login = document.querySelector('#login')
+
+login.addEventListener('click', fazerLogin)
+
+user.addEventListener('focus', function() {
+    user.style.borderColor = 'transparent';
+    senha.style.borderColor = 'transparent';
+    erroLogin.innerHTML = '';
+
+  });
+  
+  senha.addEventListener('focus', function() {
+    senha.style.borderColor = 'transparent';
+    user.style.borderColor = 'transparent';
+    erroLogin.innerHTML = '';
+
+  });
+  
+
+function fazerLogin() {
+    if (user.value == 'jonatas' && senha.value == 'jonatasmusica') {
+        window.location.href = 'file:///C:/Users/jonat/Documents/MeusProjetos2025/Bentao-2025/PW/Trabalho1/main/index..html';
+
+    } else {
+        erroLogin.innerHTML = 'preencha os campos corretamente';
+        user.style.borderColor = 'red';
+        senha.style.borderColor = 'red';
+
+    }
+}
+
